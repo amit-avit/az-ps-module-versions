63bc2d35f9536c6ff0b5feecfc7304b5ff30028d
Amit Kumar <amit-avit@users.noreply.github.com>
2021-01-20 00:06:51 +0530

    Update Find-MinModuleVersionNotInManifest.ps1

--- Find-MinModuleVersionNotInManifest.ps1
+++ Find-MinModuleVersionNotInManifest.ps1
@@ -12,4 +12,4 @@ param (
 $existingVersions = Get-Content $versionsManifestPath | ConvertFrom-Json | Select-Object -ExpandProperty version
 $minModule = Find-Module -Name $moduleName -AllVersions | Where-Object { $existingVersions -notcontains "$($_.Version.ToString())" } | Sort-Object { [System.Version]$_.Version } -Top 1
 
-return $minModule.Version.ToString()
+return $minModule.Version?.ToString()
